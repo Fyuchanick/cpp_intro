3478cb41d38bc187f63c321d8e9d27a8393f06d7
Fyuchanick <fyuchanick@gmail.com>
2017-07-03 15:04:07 -0400

    Added part of consol adlibs

--- project1.cpp
+++ project1.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-	 int num;
-	 cout<<"Enter a number"<<endl;
-	 cin>>num;
-	 cout<<num<<endl;
+	 string noun1;
+	 string noun2;
+	 string verb1;
+	 string plunoun1;
+	 string adj1;
+	 string adj2;
+	 string noun3;
+	 string noun4;
+	 string presverb1;
+	 string noun5;
+	 cout<<"Please enter a noun: "<<endl;
+	 cin>>noun1;
+	 cout<<"Please enter another noun: "<<endl;
+	 cin>>noun2;
+	 cout<<"Please enter a verb: "<<endl;
+	 cin>>verb1;
+	 cout<<"Please enter a plural noun: "<<endl;
+	 cin>>plunoun1;
+	 cout<<"Please enter an adjective: "<<endl;
+	 cin>>adj1;
+	 cout<<"Please enter another adjective:"<<endl;
+	 cin>>adj2;
+	 cout<<"Please enter a third noun: "<<endl;
+	 cin>>noun3;
+	 cout<<"Please enter a fourth noun: "<<endl;
+	 cin>>noun4;
+	 cout<<"Please enter a present-tense verb: "<<endl;
+	 cin>>presverb1;
+	 cout<<"Please enter a third noun"<<endl;
+	 cout<<"According to all known laws of "<<noun1<<", it is physically impossible for a "<<noun2<<" to "<<verb1<<". It's "<<plunoun1<<" are to small to get its "<<adj1<<" "<<adj2<<" "<<noun3<<" off the ground. The "
+	 <<noun4<<", of course, "<<presverb1<<" anyway, because "<<noun1<<"s don't care what ";
 }
